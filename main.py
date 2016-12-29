2b4768bb2c3348edc883fcff3de19f6bef2c888a
Asaf Fisher <asaffisher.dev@gmail.com>
2016-12-28 22:06:26 -0500

    Added request header text manipulations.

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import socket,sys
 from thread import *
-__port__ = 10003
+__port__ = 10005
 
     #conn.send("HTTP/1.1 200 OK\nDate: Wed, 11 Apr 2012 21:29:04 GMT\nServer: Python/6.6.6 (custom)\nContent-Type: text/html\n\n<body>ERROR</body>")
     #conn.close()
@@ -11,8 +11,18 @@ __port__ = 10003
     #WebServer Properties
 #conn.send("HTTP/1.1 200 OK\n"+"Content-Type: text/html\n" +"\n" # Important! +"<html><body>Hello World</body></html>\n")
 def prepper_web_request(conn,data,addr):
-    data.split(" ")
-    conn.send("HTTP/1.1 200 OK\n"+"Content-Type: text/html\n" +"\n" +"<html><body>Cannot access internet!</body></html>\n")
+    temp = data.split()
+    print(temp)
+    if temp[0] == 'CONNECT':
+        print "CONNECT request"
+        webaddress = (temp[1].split(':'))[0]
+        webport = (temp[1].split(':'))[1]
+        send_web_request(webaddress,webport)
+        conn.send("HTTP/1.1 200 Connection established\nProxy-agent: Python Proxy/0.1.0 Draft 1\n\n")
+
+    if temp[0] == 'GET':
+        conn.send("HTTP/1.1 200 OK\n"+"Content-Type: text/html\n" +"\n" +"<html><body><h1>It works!</h1></body></html>\n")
+
     print(data)
     conn.close()
 
